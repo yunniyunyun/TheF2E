95cf943b9493772479092e3602452e004d2e7d87
yun <ziyunn99@gmail.com>
2022-11-13 16:30:51 +0800

    feat: add menu

--- js/all.js
+++ js/all.js
@@ -1,4 +1,11 @@
-gsap.registerPlugin(ScrollTrigger, TextPlugin);
-gsap.to('.cursor', {opacity:0, duration:1, repeat: -1})
-gsap.to('.box', {x: 100, duration:1,})
-gsap.to('.box', {y: 100, duration:1, delay: 1})
+// gsap.registerPlugin(ScrollTrigger, TextPlugin);
+// gsap.to('.cursor', {opacity:0, duration:1, repeat: -1})
+// gsap.to('.box', {x: 100, duration:1,})
+// gsap.to('.box', {y: 100, duration:1, delay: 1})
+$(document).ready(function () {
+    $(".menu-btn").click(function (event) {
+        event.preventDefault();
+        $(".menu-open").toggleClass("open");
+    });
+  });
+  
